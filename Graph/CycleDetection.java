2dac9437714f6a59c45ff7f5a4d7863d4ecb9637
Grv <116830925+GRVKMR2003@users.noreply.github.com>
2025-01-09 21:02:21 +0530

    Add files via upload

---
+++ Graph/CycleDetection.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int v, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+        }
+
+        int[] ind = new int[v];
+        for (int i = 0; i < v; i++) {
+            for (int j : adj.get(i)) {
+                ind[j]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < v; i++) {
+            if (ind[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        ArrayList<Integer> arrr = new ArrayList<>();
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            arrr.add(curr);
+            for (int i : adj.get(curr)) {
+                ind[i]--;
+                if (ind[i] == 0) {
+                    q.add(i);
+                }
+            }
+        }
+
+        return arrr.size() == v;
+    }
+}
